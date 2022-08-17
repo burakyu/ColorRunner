391e834b7cbae5cba054f6d13a26354098c85c6c
Burak Yuksek <69505119+burakyu@users.noreply.github.com>
2022-08-17 13:32:12 +0300

    Refactor finalUI script

--- Assets/2_Scripts/UI/FinalUI.cs
+++ Assets/2_Scripts/UI/FinalUI.cs
@@ -8,14 +8,28 @@ public class FinalUI : MonoBehaviour
 {
     [SerializeField] Slider successPercentageSlider;
     [SerializeField] TextMeshProUGUI successPercentageText;
+    private void OnEnable()
+    {
+        EventManager.OnLevelChange.AddListener(resetSlider);
+    }
+
+    private void OnDisable()
+    {
+        EventManager.OnLevelChange.RemoveListener(resetSlider);
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (successPercentageSlider.value < ColorCompare.Instance.successPercentage / 100)
         {
-            successPercentageSlider.value += 0.5f * Time.deltaTime;
+            successPercentageSlider.value += 1f * Time.deltaTime;
             successPercentageText.text = ((int)ColorCompare.Instance.successPercentage) + "%";
         }
+    }
+
+    void resetSlider()
+    {
+        successPercentageSlider.value = 0;
     }
 }
